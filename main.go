55f466c2f4248851b94305358ab152f42c6bc017
Andrey <medvedevandrey860@gmail.com>
2025-05-09 03:55:28 +0300

    createAllValidators

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"os"
+	"regexp"
+	"strconv"
 	"strings"
 	"unicode"
 )
 
-// допустимые символы для Email
-func allowedChar(chars rune) bool {
+func allowedCharEmail(chars rune) bool {
 	return (chars >= 'a' && chars <= 'z') ||
 		(chars >= 'A' && chars <= 'Z') ||
 		(chars >= '0' && chars <= '9') ||
@@ -20,47 +23,55 @@ func optionMenu() {
 	fmt.Println("3. Перевірка телефонного номера")
 	fmt.Println("4. Перевірка IP-адреси")
 	fmt.Println("5. Перевірка URL-адреси")
+	fmt.Println("6. Перевірка Дати")
 	fmt.Println("0. Вихід ")
 }
 
 func validEmail(email string) error {
-	// створення помилки
-	err := errors.New("Email не валідний!")
 
-	//перевірка введеного email на наявність символа "@" чи пробілів
-	if strings.Count(email, "@") != 1 || strings.Count(email, " ") != 0 {
-		return err
+	var reasons []string
+
+	if strings.Count(email, "@") == 0 {
+		reasons = append(reasons, "- Нема символа @")
 	}
-	//розділення ємейлу на дві частини
+	if strings.Count(email, "@") != 1 {
+		reasons = append(reasons, "- Не повинно бути біль ніж 1 символ @")
+	}
+	if strings.Count(email, " ") != 0 {
+		reasons = append(reasons, "- Не повинно бути пробілів ")
+	}
+
 	parts := strings.Split(email, "@")
 
-	//перевірка на наявність символів в локальній та доменній частинах
-	if len(parts[0]) == 0 || len(parts[1]) == 0 {
-		return err
+	if len(parts[0]) == 0 {
+		reasons = append(reasons, "- В локаліній частини немає символів")
+	}
+	if len(parts[1]) == 0 {
+		reasons = append(reasons, "- В доменній частини немає символів")
 	}
 
-	//розділення доменної частини
 	domen := strings.Split(parts[1], ".")
 
-	//перевірка доменной частини на наявність крапки
 	if len(domen) <= 1 {
-		return err
+		reasons = append(reasons, "- Немає крапки в доменній частині")
 	}
 
-	//останній єлемент в доменній частини пілся крапки
-	last := domen[len(domen)-1]
-	//перевірка на довжину останного елемента
+	last := strings.TrimSpace(domen[len(domen)-1])
+
 	if len(last) < 2 || len(last) > 6 {
-		return err
+		reasons = append(reasons, "- Домен верхнього рівня повинен мати довжину від 2 до 6 символів ")
 	}
-	//перевірка на допустимість символів для Локаліної частини
+
 	local := parts[0]
 	for _, aChars := range local {
-		if !allowedChar(aChars) {
-			return err
+		if !allowedCharEmail(aChars) {
+			reasons = append(reasons, "- Доменна частина не повинна мати недопустимий символ \n допустимі символи: літери, цифри, крапки, дефіси, підкреслення")
 		}
 	}
 
+	if len(reasons) > 0 {
+		return errors.New("Email невалідний! Причини: \n" + strings.Join(reasons, "\n"))
+	}
 	return nil
 }
 
@@ -110,9 +121,222 @@ func validPass(password string) error {
 
 	return nil
 }
+
+func validPhone(phonNum string) error {
+	var reasons []string
+	UAoperators := []string{"039", "050", "063", "066", "067", "068", "091", "092", "093", "094", "095", "096", "097", "098", "099"}
+	var onlydigits, validOp bool
+	if !strings.HasPrefix(phonNum, "+") {
+		reasons = append(reasons, "- Повино щоб Номер Починався з +")
+	}
+	if strings.Count(phonNum, "+") > 1 {
+		reasons = append(reasons, "- Номер повинен містити + тільки з початку")
+	}
+
+	phonNum = strings.TrimSpace(phonNum)
+	for _, char := range phonNum {
+		if !strings.ContainsAny(string(char), "1234567890 -+()") {
+			reasons = append(reasons, "- Номер містить недопустимі символи")
+			break
+		}
+	}
+
+	lenphone := regexp.MustCompile(`\D`)
+	onlyDPhone := lenphone.ReplaceAllString(phonNum, "")
+	if len(onlyDPhone) > 15 {
+		reasons = append(reasons, "- У номері більше символів чим треба")
+	}
+	if len(onlyDPhone) < 10 {
+		reasons = append(reasons, "- У номері менше символів чим треба")
+	}
+	for _, char := range onlyDPhone {
+		if unicode.IsDigit(char) {
+			onlydigits = true
+		}
+	}
+
+	if strings.HasPrefix(onlyDPhone, "380") {
+		onlyDPhone = onlyDPhone[2:]
+		operCode := onlyDPhone[:3]
+		for _, code := range UAoperators {
+			if operCode == code {
+				validOp = true
+				break
+			}
+		}
+	}
+
+	if !onlydigits {
+		reasons = append(reasons, "- В номері не повино бути літер")
+	}
+	if !validOp {
+		reasons = append(reasons, "- Номер повинен бути з правильним оператором ")
+	}
+	if len(reasons) > 0 {
+		return errors.New("Телефон не Валідний! Причини: \n" + strings.Join(reasons, "\n"))
+	}
+
+	return nil
+}
+
+func validIp(ip string) error {
+	var reasons []string
+	parts := strings.Split(ip, ".")
+
+	if len(parts) != 4 {
+		reasons = append(reasons, "- Неверный формат ввода повино бути 4 части")
+	}
+
+	for i, part := range parts {
+		if part == "" {
+			reasons = append(reasons, fmt.Sprintf("- Частина %d порожня ", i+1))
+		}
+
+		if strings.Count(part, " ") != 0 {
+			reasons = append(reasons, "- Ip-Адреса не повина мати пробілів")
+		}
+
+		part = strings.TrimSpace(part)
+
+		num, _ := strconv.Atoi(part)
+		if !strings.ContainsAny(part, "1234567890") {
+			reasons = append(reasons, fmt.Sprintf("- Частина %d содержит неправильний символ ", i+1))
+			continue
+		}
+
+		if num < 0 || num > 255 {
+			reasons = append(reasons, fmt.Sprintf("- Частина %d поза діапазоном 0-255 ", i+1))
+		}
+		if len(part) > 1 && part[0] == '0' {
+			reasons = append(reasons, fmt.Sprintf("- Частина %d містить 0 перед числом: ", i+1))
+		}
+	}
+	ip = strings.TrimSpace(ip)
+	if len(ip) < 7 || len(ip) > 15 {
+		reasons = append(reasons, "- Довжина має бути від 7 до 15 символів ")
+	}
+
+	if len(reasons) > 0 {
+		return errors.New("Ip-Адреса не Валідна! Причини: \n" + strings.Join(reasons, "\n"))
+	}
+	return nil
+}
+
+func allowedCharUrl(chars rune) bool {
+	return (chars >= 'a' && chars <= 'z') ||
+		(chars >= 'A' && chars <= 'Z') ||
+		(chars >= '0' && chars <= '9') ||
+		chars == '.' || chars == '-'
+}
+func validUrl(url string) error {
+	var reasons []string
+
+	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+		reasons = append(reasons, "- Немає протокола http:// або https:// ")
+	}
+
+	if strings.Count(url, " ") != 0 {
+		reasons = append(reasons, "- Не повинно бути пробілів у домені")
+	}
+
+	var remPref string
+	if strings.HasPrefix(url, "http://") {
+		remPref = strings.TrimPrefix(url, "http://")
+	} else if strings.HasPrefix(url, "https://") {
+		remPref = strings.TrimPrefix(url, "https://")
+	}
+
+	if !strings.Contains(remPref, ".") {
+		reasons = append(reasons, "- В доменній частині немає точки ")
+	}
+	remPref = strings.TrimSpace(remPref)
+	for _, chars := range remPref {
+		if !allowedCharUrl(chars) {
+			reasons = append(reasons, "- Доменне Ім'я має неправильний формат \n правильний формат: літери, цифри, дефіси")
+			break
+		}
+	}
+
+	if len(reasons) > 0 {
+		return errors.New("Url-адреса не Валідна! Причини: \n" + strings.Join(reasons, "\n"))
+	}
+	return nil
+}
+
+func leapYear(year int) bool {
+	return (year%4 == 0 && year&100 != 0) || year%400 == 0
+}
+func validDate(date string) error {
+	var reasons []string
+
+	if strings.Count(date, " ") != 0 {
+		reasons = append(reasons, "- Не повинно бути пробілів у даті")
+	}
+	date = strings.TrimSpace(date)
+	for _, char := range date {
+		if !strings.ContainsAny(string(char), "1234567890-./") {
+			reasons = append(reasons, "- Дата містить недопустимі символи \n допустимі символи: - . /")
+			break
+		}
+	}
+	var sep string
+	if strings.Contains(date, "-") {
+		sep = "-"
+	} else if strings.Contains(date, "/") {
+		sep = "/"
+	} else if strings.Contains(date, ".") {
+		sep = "."
+	} else {
+		reasons = append(reasons, "- Неправельний формат вводу")
+	}
+
+	parts := strings.Split(date, sep)
+
+	if len(parts) != 3 {
+		reasons = append(reasons, "- Неправельний формат вводу повино бути 3 части")
+	}
+	day, _ := strconv.Atoi(parts[0])
+	month, _ := strconv.Atoi(parts[1])
+	year, _ := strconv.Atoi(parts[2])
+
+	if month < 1 || month > 12 {
+		reasons = append(reasons, "- Невірний місяці")
+	}
+
+	var dayInMon int
+
+	switch month {
+	case 1, 3, 5, 7, 8, 10, 12:
+		dayInMon = 31
+	case 4, 6, 9, 11:
+		dayInMon = 30
+	case 2:
+		if leapYear(year) {
+			dayInMon = 29
+		} else {
+			dayInMon = 28
+		}
+	}
+
+	if day < 1 || day > dayInMon {
+		reasons = append(reasons, "-  Невірний день Місяця")
+	}
+
+	if year < 1900 || year > 9999 {
+		reasons = append(reasons, "-  Невірний рік")
+	}
+
+	if len(reasons) > 0 {
+		return errors.New("Дата не Валідна! Причини: \n" + strings.Join(reasons, "\n"))
+	}
+	return nil
+}
+
 func main() {
 	var choise int
-	var email, password string
+	var email, password, phoneNum, ip, url, date string
+
+	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Println("\nВиберіть опцію:")
 		optionMenu()
@@ -125,7 +349,7 @@ func main() {
 			return
 		case 1:
 			fmt.Print("Введіть email-адресу: ")
-			fmt.Scanln(&email)
+			email, _ = reader.ReadString('\n')
 			err := validEmail(email)
 			if err != nil {
 				fmt.Println("Результат: ", err)
@@ -134,7 +358,7 @@ func main() {
 			}
 		case 2:
 			fmt.Print("Введіть пароль: ")
-			fmt.Scanln(&password)
+			password, _ = reader.ReadString('\n')
 			err := validPass(password)
 			if err != nil {
 				fmt.Println("Результат: ", err)
@@ -142,13 +366,43 @@ func main() {
 				fmt.Println("Результат: Пароль надійний!")
 			}
 		case 3:
-			fmt.Println("Ваш вибір: ", choise)
+			fmt.Print("Введіть Номер Телефону: ")
+			phoneNum, _ = reader.ReadString('\n')
+			err := validPhone(phoneNum)
+			if err != nil {
+				fmt.Println("Результат: ", err)
+			} else {
+				fmt.Println("Результат: Номер валідний!")
+			}
 		case 4:
-			fmt.Println("Ваш вибір: ", choise)
+			fmt.Print("Введіть Ip-Адресу: ")
+			ip, _ = reader.ReadString('\n')
+			err := validIp(ip)
+			if err != nil {
+				fmt.Println("Результат: ", err)
+			} else {
+				fmt.Println("Результат: Ip-Адреса Валідна!")
+			}
 		case 5:
-			fmt.Println("Ваш вибір: ", choise)
+			fmt.Print("Введіть Url-Адресу: ")
+			url, _ = reader.ReadString('\n')
+			err := validUrl(url)
+			if err != nil {
+				fmt.Println("Результат: ", err)
+			} else {
+				fmt.Println("Результат: Url-Адреса Валідна!")
+			}
+		case 6:
+			fmt.Print("Введіть Дату: ")
+			date, _ = reader.ReadString('\n')
+			err := validDate(date)
+			if err != nil {
+				fmt.Println("Результат: ", err)
+			} else {
+				fmt.Println("Результат: Дата Валідна!")
+			}
 		default:
-			fmt.Println("Такого Виору не існує")
+			fmt.Println("Такого Вибору не існує")
 		}
 	}
 }
